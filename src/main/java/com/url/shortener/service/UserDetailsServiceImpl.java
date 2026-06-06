b48c0ca51cd05f697e9464dfb58553b95329bbca
manishachoudhary11 <manisha702091@gmail.com>
2026-06-06 17:01:49 +0530

    Initial URL Shortener setup with JWT authentication in progress

---
+++ src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.url.shortener.service;
+
+import com.url.shortener.models.User;
+import com.url.shortener.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        return null;
+    }
+}
